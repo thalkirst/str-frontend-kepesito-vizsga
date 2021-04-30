0d636843363c22e04f895b20cd01336e362c12b1
Thalkirst <peter.bonyai@gmail.com>
2021-04-30 12:58:45 +0200

    done

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,23 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    
+    //format outer links//
+    const links = document.querySelectorAll("#link-list > a");
+    const emphasizeOuterLinks = link => {
+        if (link.innerHTML.toLowerCase().includes("outer-link")) {
+            link.setAttribute('target', '_blank')
+            link.innerHTML = `<strong>${link.innerHTML}</strong>`
+        }
+    }
+    links.forEach(link => emphasizeOuterLinks(link));
 
+    //format navigation links//
+    const navbar = document.querySelector("#link-list");
+    navbar.style.display = 'flex';
+    navbar.style.flexDirection = 'column';
+    navbar.style.margin = '0px auto';
+    navbar.style.width = '30%';
+    navbar.style.border = '1px solid blue';
+    navbar.style.padding = '16px';
+}
 
 export { changeOuterLinks };
